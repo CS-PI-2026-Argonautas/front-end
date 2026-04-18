80ad54521b0bd95434df2b4afaedcbe2b11110d3
Gigii55 <gigibernadelli001@gmail.com>
2026-04-17 21:37:37 -0300

    feat(authentication.dart): estilizacao campos de input e botao

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -7,10 +7,21 @@ class Authentication extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.blue,
-        body: SafeArea(
+        body: Container(
+          decoration: BoxDecoration(
+      gradient: LinearGradient(
+        colors: [
+          Colors.blue,
+          const Color.fromARGB(255, 0, 59, 86),
+        ],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      ),
+    ),
           child: Padding(
             padding: EdgeInsets.all(30),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset(
               'lib/assets/images/LogoEmpresa.png',
@@ -23,26 +34,34 @@ class Authentication extends StatelessWidget {
                 filled: true,
                 prefixIcon: Icon(Icons.person),
                 border: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.blue
-                ),
+                borderRadius: BorderRadius.circular(14),
               ),),
               ),
               SizedBox(height: 20,),
-              TextField(decoration: InputDecoration(
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
                 hintText: 'Senha',
                 filled: true,
                 prefixIcon: Icon(Icons.password),
-                border: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.blue
-                ),
+                border: OutlineInputBorder( 
+                borderRadius: BorderRadius.circular(14),
               ),),
               ),
               SizedBox(height: 20,),
+              ElevatedButton.icon(
+              onPressed:(){},
+              icon: Icon(Icons.login),
+              label: Text('Entrar', style: TextStyle(fontSize: 18),),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: const Color.fromARGB(255, 13, 35, 54),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14),
+                ),
+              ),
               
-          
-              ElevatedButton(onPressed:(){}, child: Text('Entrar')),
+              ),
               ],
                 ),
               ),
