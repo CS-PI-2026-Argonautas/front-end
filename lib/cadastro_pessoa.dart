16978c7fdde522cc2114924c22082c08779414d1
Murilo <murilorolim2005@gmail.com>
2026-04-24 21:47:28 -0300

    correção de uns imports não utilizados

--- lib/cadastro_pessoa.dart
+++ lib/cadastro_pessoa.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/style/inputDecorationStyles.dart';
-import 'package:frontend/style/ColorScheme.dart';
-import 'package:frontend/style/icon.dart';
-
-
 
 class PersonRegistration extends StatefulWidget {
   const PersonRegistration({super.key});
@@ -20,81 +16,69 @@ class _PersonRegistrationState extends State<PersonRegistration> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: const Color.fromARGB(255, 64, 126, 207),
-        appBar: AppBar(
-          title: Row(),      
-        ),
-        body: 
-        Card(child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-          child: Center(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    TextFormField(
-                      decoration: inputDecorationStyle.copyWith(
-                        labelText: 'Nome',
+        appBar: AppBar(title: Row()),
+        body: Card(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+            child: Center(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      TextFormField(
+                        decoration: inputDecorationStyle.copyWith(
+                          labelText: 'Nome',
+                        ),
                       ),
+                    ],
+                  ),
+
+                  SizedBox(height: 15),
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
                     ),
-                  ],
-                ),
-              
-                
-                SizedBox(height: 15),
-
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    enabledBorder: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(),
                   ),
-                ),
-                SizedBox(height: 15),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'CPF',
-                    border: OutlineInputBorder(),
+                  SizedBox(height: 15),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'CPF',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 15),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                    border: OutlineInputBorder(),
+                  SizedBox(height: 15),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
                   ),
-                  obscureText: true,
-                ),
-                SizedBox(height: 15),
-                ElevatedButton(
-                  onPressed: () {
-                    
-                  },
-                  child: Text('Cadastrar'),
-                ),
-              ],
-
+                  SizedBox(height: 15),
+                  ElevatedButton(onPressed: () {}, child: Text('Cadastrar')),
+                ],
+              ),
             ),
           ),
-        )),
         ),
-        
-
-      );
+      ),
+    );
   }
-} 
+}
 
-class _PersonRegistrationState2 extends State<PersonRegistration>{
+class _PersonRegistrationState2 extends State<PersonRegistration> {
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
 
-class _PersonRegistrationState3 extends State<PersonRegistration>{
+class _PersonRegistrationState3 extends State<PersonRegistration> {
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
-
-
