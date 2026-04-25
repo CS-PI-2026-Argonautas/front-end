9118f56738343dbc549293911c09c065b63cbdd9
Gigii55 <gigibernadelli001@gmail.com>
2026-04-24 21:57:03 -0300

    fixing code indentation

--- lib/pages/authentication/authentication.dart
+++ lib/pages/authentication/authentication.dart
@@ -3,75 +3,81 @@ import 'package:flutter/material.dart';
 class Authentication extends StatefulWidget {
   const Authentication({super.key});
 
-@override
-State<Authentication> createState() => _AuthenticationState();
+  @override
+  State<Authentication> createState() => _AuthenticationState();
 }
 
 class _AuthenticationState extends State<Authentication> {
-  
   bool _isPasswordVisible = false;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blue,
-        body: Container(
-          decoration: BoxDecoration(
-      gradient: LinearGradient(
-        colors: [
-          Colors.blue,
-          const Color.fromARGB(255, 0, 59, 86),
-        ],
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-      ),
-    ),
-          child: Padding(
-            padding: EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-              'lib/assets/images/LogoEmpresa.png',
-              width: 350,
-              height: 180,
+      backgroundColor: Colors.blue,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue, const Color.fromARGB(255, 0, 59, 86)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
-          
-                TextField(decoration: InputDecoration(
-                hintText: 'Usuário',
-                filled: true,
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(14),
-              ),),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'lib/assets/images/LogoEmpresa.png',
+                width: 350,
+                height: 180,
               ),
 
-              SizedBox(height: 20,),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Usuário',
+                  filled: true,
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20),
               TextField(
                 obscureText: !_isPasswordVisible,
                 decoration: InputDecoration(
-                hintText: 'Senha',
-                filled: true,
-                prefixIcon: Icon(Icons.password),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _isPasswordVisible?Icons.visibility:Icons.visibility_off,),
-                onPressed:() {
-                  setState(() {
-                    _isPasswordVisible = !_isPasswordVisible;
-                  });
-                },
+                  hintText: 'Senha',
+                  filled: true,
+                  prefixIcon: Icon(Icons.password),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                ),
               ),
-                border: OutlineInputBorder( 
-                borderRadius: BorderRadius.circular(14),
-              ),),
-              ),
-              SizedBox(height: 20,),
+              SizedBox(height: 20),
               GestureDetector(
-                onTap: () {}, child: Text("Esqueceu senha?", style: TextStyle(color: Colors.white),),
+                onTap: () {},
+                child: Text(
+                  "Esqueceu senha?",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-              SizedBox(height: 150,),
-                            SizedBox(
+              SizedBox(height: 150),
+              SizedBox(
                 height: 50,
                 width: 120,
                 child: ElevatedButton.icon(
@@ -82,33 +88,31 @@ class _AuthenticationState extends State<Authentication> {
                     backgroundColor: Colors.white,
                     foregroundColor: const Color.fromARGB(255, 13, 35, 54),
                     shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(14),
+                      borderRadius: BorderRadius.circular(14),
                     ),
                   ),
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(height: 20),
               GestureDetector(
-                onTap: () {}, child: Text.rich(
-              TextSpan(
-                style: const TextStyle(color: Colors.white, fontSize: 16),
-                children: [
-                  const TextSpan(text: "Não tem uma conta? "),
-                  const TextSpan(
-                    text: "Cadastre-se",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
+                onTap: () {},
+                child: Text.rich(
+                  TextSpan(
+                    style: const TextStyle(color: Colors.white, fontSize: 16),
+                    children: [
+                      const TextSpan(text: "Não tem uma conta? "),
+                      const TextSpan(
+                        text: "Cadastre-se",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                ],
-              ),),
-              ),
-              ],
                 ),
               ),
-              ),
-              );
-              
-          }
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
-
