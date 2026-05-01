e0126c449fe9d84ac03dee485ba01ff79b4bf798
Felipe Fabris Leite Victorino <80400964+Felipe-Victorino@users.noreply.github.com>
2026-04-30 21:32:10 -0300

    Revert "Fix person registration files"

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/pages/PersonRegistration2.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: PersonRegistration2(),
-      theme: ThemeData(
-        colorScheme: .fromSeed(
-          seedColor: const Color.fromARGB(255, 64, 126, 207),
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
         ),
       ),
     );
--- lib/style/icon.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:frontend/style/colorScheme.dart';
-
-final iconDecorationStyle = IconThemeData(
-  color: colorScheme.primary,
-);
