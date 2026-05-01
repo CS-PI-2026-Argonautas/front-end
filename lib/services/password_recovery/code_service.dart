5262cb305b31cb293049ae23ffa3bd3e3d302078
Isaq-11 <isaquecarvalhoxavier@gmail.com>
2026-05-01 18:39:54 -0300

    feat(gerador de codigo de redefinicao de senha): adicionados metodos para criar e validar o codigo necessario pra alterar senha

---
+++ lib/services/password_recovery/code_service.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+class CodeService {
+  String? _code;
+  DateTime? _expiration;
+
+  String generateCode(){
+    Random random = Random();
+    return(100000 + random.nextInt(900000)).toString();
+  }
+
+  void createCode(){
+    _code = generateCode();
+    _expiration = DateTime.now().add(Duration(minutes: 5));
+  }
+
+  bool validateCode(String value){
+    if(_code == null || _expiration == null) return false;
+
+    if(DateTime.now().isAfter(_expiration!)) return false;
+
+    return value == _code ? true : false;
+  }
+
+  String? get code => _code;
+  DateTime? get expiration => _expiration;
+
+}
+
