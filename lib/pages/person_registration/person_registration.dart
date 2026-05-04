729c2f117c384c56a48c32ef4d6df596c530f8ae
MariaBruning15 <mariaeduardafbruning@gmail.com>
2026-05-04 11:40:57 -0300

    feat(person_registration): cpf/cnpj mask text

--- lib/pages/person_registration/person_registration.dart
+++ lib/pages/person_registration/person_registration.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/style/colorScheme.dart' as custom_colors;
 import 'package:frontend/pages/person_registration/person_registration2.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 class PersonRegistration extends StatefulWidget {
   const PersonRegistration({super.key});
 
@@ -10,6 +11,17 @@ class PersonRegistration extends StatefulWidget {
 
 class _PersonRegistrationState1 extends State<PersonRegistration> {
   final _formKey = GlobalKey<FormState>();
+  bool _isPessoaFisica = false;
+
+  final _cpfFormatter = MaskTextInputFormatter(
+    mask: '###.###.###-##', 
+    filter: {"#": RegExp(r'[0-9]')}
+  );
+
+  final _cnpjFormatter = MaskTextInputFormatter(
+    mask: '##.###.###/####-##', 
+    filter: {"#": RegExp(r'[0-9]')}
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +33,8 @@ class _PersonRegistrationState1 extends State<PersonRegistration> {
           child: Center(
             child: ConstrainedBox(
               constraints: const BoxConstraints(maxWidth: 650),
+              child: Form(
+                key: _formKey,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -43,22 +57,36 @@ class _PersonRegistrationState1 extends State<PersonRegistration> {
                       },
                   ),
                   const SizedBox(height: 20),
-                  _buildFieldLabel(Icons.badge_outlined, "CNPJ *"),
-                  TextFormField(decoration: _inputStyle("132123123123"), keyboardType: TextInputType.number,
+                  _buildFieldLabel(
+                    Icons.badge_outlined, 
+                    _isPessoaFisica ? "CPF *" : "CNPJ *" // Label dinâmico
+                  ),
+                  TextFormField(
+                    key: ValueKey(_isPessoaFisica), // Força o Flutter a redesenhar o campo ao mudar o tipo
+                    decoration: _inputStyle(_isPessoaFisica ? "000.000.000-00" : "00.000.000/0000-00"),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [_isPessoaFisica ? _cpfFormatter : _cnpjFormatter], // Máscara dinâmica
                     validator: (value) {
                       if (value == null || value.isEmpty) {
-                        return 'Por favor, informe o cnpj';
-                        }
-                       return null;
-                      },
+                        return 'Por favor, informe o ${_isPessoaFisica ? "CPF" : "CNPJ"}';
+                      }
+                      return null;
+                     },
                   ),
                   const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Checkbox(value: false, onChanged: (val) {}),
-                      const Text("Pessoa física?"),
-                    ],
-                  ),
+                 Row(
+  children: [
+    Checkbox(
+      value: _isPessoaFisica, 
+      onChanged: (bool? value) {
+        setState(() {
+          _isPessoaFisica = value ?? false; // Atualiza o estado
+        });
+      }
+    ),
+    const Text("Pessoa física?"),
+  ],
+),
                   const SizedBox(height: 30),
                   SizedBox(
                     width: double.infinity,
@@ -69,12 +97,21 @@ class _PersonRegistrationState1 extends State<PersonRegistration> {
                           Navigator.push(
                             context, MaterialPageRoute(builder: (context) => const PersonRegistration2())
                           );
+                        }else{
+                          ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: const Text('Preencha todos os campos obrigatórios!'),
+                          backgroundColor: custom_colors.colorScheme.error, // Sua cor de erro[cite: 2]
+                          behavior: SnackBarBehavior.floating,
+                        ),
+                        );
                         }
                       },
                       child: const Text("Próximo"),
                     )
                   ),
                 ],
+              )
               )
             ),
           ),
