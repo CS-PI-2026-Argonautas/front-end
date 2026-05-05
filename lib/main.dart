b3cc390d59955ad383faf8f1500841aaea668e2f
Isaq-11 <isaquecarvalhoxavier@gmail.com>
2026-05-05 09:49:52 -0300

    fix(main): pego main da branch dev e colocado na minha branch para evitar conflito

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,21 @@
 import 'package:flutter/material.dart';
-
-import 'package:frontend/pages/password_recovery/user_information.dart';
-import 'package:frontend/pages/person_registration.dart';
-import 'package:frontend/pages/product_form.dart';
-import 'package:frontend/pages/product_header.dart';
-import 'package:frontend/pages/product_registration.dart';
+import 'package:frontend/pages/PersonRegistration2.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Recuperação de Senha',
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: UserInformation(),
+      home: PersonRegistration2(),
+      theme: ThemeData(
+        colorScheme: .fromSeed(
+          seedColor: const Color.fromARGB(255, 64, 126, 207),
+        ),
+      ),
     );
   }
 }
-
-
-
