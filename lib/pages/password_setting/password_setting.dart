f8baf0bbae3408f7a37b186e2519c95bc9a43876
Isaq-11 <isaquecarvalhoxavier@gmail.com>
2026-05-22 22:20:43 -0300

    feat(enunciado): adicionado enunciado juntamente com exemplo da dica

--- lib/pages/password_setting/password_setting.dart
+++ lib/pages/password_setting/password_setting.dart
@@ -22,6 +22,38 @@ class _PasswordSettingState extends State<PasswordSetting>{
           ),
         ),
       ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              child: _TipText(),
+            ),
+            SizedBox(),
+            SizedBox(),
+            SizedBox(),
+            SizedBox(),
+            SizedBox(),
+            SizedBox(),
+            SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TipText extends StatelessWidget{
+
+  String tipText = 'Dica: use frases-passe ao invés de senhas padrões para criar credenciais mais longas, seguras e fáceis de memorizar.';
+  String exampleText = 'Exemplo: cachorro#morde#pelucia@23';
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(tipText),
+        SizedBox(height: 10,),
+        Text(exampleText),
+      ],
     );
   }
 }
