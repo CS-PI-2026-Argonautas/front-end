b72c7384f4d2d16b06c0ac91533879d04f6036da
MariaBruning15 <mariaeduardafbruning@gmail.com>
2026-05-22 22:10:04 -0300

    feat(person_alteration): person alteration base code and test file

---
+++ lib/pages/person_alteration/test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/pages/person_alteration/person_alteration.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PersonAlteration(),
+    );
+  }
+}
