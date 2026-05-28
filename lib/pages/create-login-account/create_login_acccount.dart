db9bf478051212732bf031aa7fef39c272b63453
Gigii55 <gigibernadelli001@gmail.com>
2026-05-27 22:19:52 -0300

    feat(create_login_account.dart): styling form

--- lib/pages/create-login-account/create_login_acccount.dart
+++ lib/pages/create-login-account/create_login_acccount.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/style/ColorScheme.dart' as custom_colors;
 
-class CreateLoginAccount extends StatefulWidget{
-  const CreateLoginAccount ({super.key});
+class CreateLoginAccount extends StatefulWidget {
+  const CreateLoginAccount({super.key});
 
   @override
-  State<CreateLoginAccount> createState() => CreateLoginAccountState(
-  );
+  State<CreateLoginAccount> createState() => CreateLoginAccountState();
 }
 
 class CreateLoginAccountState extends State<CreateLoginAccount> {
-final colors = custom_colors.colorScheme;
+  final colors = custom_colors.colorScheme;
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-  body: Container(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [colors.primary, Color.fromARGB(255, 0, 59, 86)],
@@ -25,113 +24,209 @@ return Scaffold(
         ),
         child: Center(
           child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 50),
-      
-          child: Form(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                'Nome *',
-                style: TextStyle(
-                fontSize: 15,
-              
-                ),
-                ),
-                SizedBox(height: 5),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Nome',
-                    filled: true,
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(14),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 40),
-                const Text(
-                'Sobrenome *',
-                style: TextStyle(
-                fontSize: 15,
-            
-                ),
-                ),
-                SizedBox(height: 5),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Sobrenome ',
-                    filled: true,
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(14),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 40),
-                const Text(
-                'Nome de usuário *',
-                style: TextStyle(
-                fontSize: 15,
-                
-                ),
-                ),
-                SizedBox(height: 5),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'nome.sobrenome',
-                    filled: true,
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(14),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 40),
-                const Text(
-                'Email *',
-                style: TextStyle(
-                fontSize: 15,
-              
-                ),
-                ),
-                SizedBox(height: 5),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'nome@exemplo.com',
-                    filled: true,
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(14),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 40),
-                Center( 
-                  child: ElevatedButton.icon(
-                      onPressed: (){},
-                      style: ElevatedButton.styleFrom(
-                        elevation: 3,
-                        backgroundColor: colors.primary,
-                        foregroundColor: colors.onSecondary,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(14),
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 50),
+            child: Card(
+              color: Colors.white,
+              elevation: 8,
+              shadowColor: Colors.black26,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(22),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.badge, size: 20, color: colors.primary),
+                          const SizedBox(width: 8),
+                          Text(
+                            "Nome *",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: colors.onSurface,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 5),
+
+                      TextFormField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                          fillColor: colors.surface,
+                          hintText: 'Nome',
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                            borderSide: BorderSide(
+                              color: colors.primary,
+                              width: 2.0,
+                            ),
+                          ),
                         ),
                       ),
-                      icon: const Icon(Icons.arrow_forward_outlined),
-                      label: const Text(
-                        "Prosseguir",
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                      SizedBox(height: 40),
+
+                      Row(
+                        children: [
+                          Icon(Icons.badge, size: 20, color: colors.primary),
+                          const SizedBox(width: 8),
+                          Text(
+                            "Sobrenome *",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: colors.onSurface,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
+                      SizedBox(height: 5),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                          fillColor: colors.surface,
+                          hintText: 'Sobrenome ',
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                            borderSide: BorderSide(
+                              color: colors.primary,
+                              width: 2.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Row(
+                        children: [
+                          Icon(Icons.person_3, size: 20, color: colors.primary),
+                          const SizedBox(width: 8),
+                          Text(
+                            "Nome usuário *",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: colors.onSurface,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 5),
+
+                      TextFormField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                            borderSide: BorderSide(
+                              color: colors.primary,
+                              width: 2.0,
+                            ),
+                          ),
+                          hintText: 'nome.sobrenome',
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 40),
+                      Row(
+                        children: [
+                          Icon(Icons.email, size: 20, color: colors.primary),
+                          const SizedBox(width: 8),
+                          Text(
+                            "Email *",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: colors.onSurface,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 5),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                              width: 1.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                            borderSide: BorderSide(
+                              color: colors.primary,
+                              width: 2.0,
+                            ),
+                          ),
+                          fillColor: colors.surface,
+                          hintText: 'nome@exemplo.com',
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Center(
+                        child: ElevatedButton.icon(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            elevation: 3,
+                            backgroundColor: colors.primary,
+                            foregroundColor: colors.onSecondary,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(14),
+                            ),
+                          ),
+                          icon: const Icon(Icons.arrow_forward_outlined),
+                          label: const Text(
+                            "Prosseguir",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-              ],
-          ),
-          ),
+                ),
+              ),
+            ),
           ),
         ),
-  ),
-);
+      ),
+    );
+  }
 }
-}
-
