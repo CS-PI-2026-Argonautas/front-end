ba82a112599db57a11d4582df0f7d61cb4b9e77d
Isaq-11 <isaquecarvalhoxavier@gmail.com>
2026-06-26 21:25:48 -0300

    feat(utils/, tela cadastro de senha): adicionada verificacao no validators.dart, feita a componentizacao da forca da senha e a utilizando na tela de cadastro de senha

--- lib/utils/password_recovery/validators.dart
+++ lib/utils/password_recovery/validators.dart
@@ -3,18 +3,23 @@ String? requiredField(String? value) {
   return null;
 }
 
-String? passwordValidator(String? value){
+String? passwordValidator(String? value, {String? comparisonValue, String? nameComparisonValue = "ao valor de comparação"}){
   if(value == null || value.trim().isEmpty) return 'Campo obrigatório';
   
   if(value.length < 8) return 'A senha deve ter no mínimo 8 caracteres';
 
+  if(value == comparisonValue) return 'A senha não pode ser igual ${nameComparisonValue}';
+
   return null;
 }
 
-String? confirmPassword (String? value, String originalPassword){
+String? confirmPassword (String? value, String originalPassword, {String? comparisonValue, String? nameComparisonValue = "ao valor de comparação"}){
   if(value == null || value.trim().isEmpty) return 'Campo obrigatório';
 
   if(value != originalPassword) return 'As senhas não coincidem';
+  
+  if(value == comparisonValue) return 'A senha não pode ser igual ${nameComparisonValue}';
+
 
   return null;
 }
