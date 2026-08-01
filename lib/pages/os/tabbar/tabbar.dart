a531ae460d163a6b32d497446b0408d2f4ac1809
Isaq-11 <isaquecarvalhoxavier@gmail.com>
2026-07-31 22:20:03 -0300

    feat(tabbar): adicionado primeira aba de teste

--- lib/pages/os/tabbar/tabbar.dart
+++ lib/pages/os/tabbar/tabbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/widgets/header.dart';
+import 'package:frontend/style/ColorScheme.dart' as custom_colors;
 
 class Tabbar extends StatefulWidget{
 
@@ -11,12 +12,43 @@ class Tabbar extends StatefulWidget{
   State<Tabbar> createState() => _TabbarState();
 }
 
-class _TabbarState extends State<Tabbar>{
+class _TabbarState extends State<Tabbar> with TickerProviderStateMixin{
+
+  late final TabController _tabController;
+
+  final colors = custom_colors.colorScheme;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 1, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: Header(onBack: () =>  Navigator.pop(context), title: Text('OS nº ${widget.serviceOrderNumber}')),
-      );
+    return DefaultTabController(
+      length: 1, 
+      child: Scaffold(
+        appBar: AppBar(
+          // backgroundColor: colors.surface,
+          title: Text('OS nº ${widget.serviceOrderNumber}'),
+          bottom: TabBar(
+            tabs: <Widget> [
+              Tab(icon: Icon(Icons.note_add_outlined), text: 'Dados',)
+            ]
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Center(child: Text('aqui serao os dados'),)
+          ]),
+      ),
+    );
   }
 }
