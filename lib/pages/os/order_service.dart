b97c92a6446fa829a77343deba08456f31fd5048
Murilo <murilorolim2005@gmail.com>
2026-08-14 20:11:26 -0300

    order_serive.dart:feat(adcional do slidable para poder apagar o card da OS)

--- lib/pages/os/order_service.dart
+++ lib/pages/os/order_service.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/style/ColorScheme.dart' as custom_colors;
+import 'package:flutter_slidable/flutter_slidable.dart';
+
 class OrderService extends StatefulWidget {
   const OrderService({super.key});
 
@@ -11,31 +13,11 @@ class _OrderService extends State<OrderService> {
   final colors = custom_colors.colorScheme;
 
   final List<Map<String, dynamic>> pecasDisponiveis = [
-    {
-      "id": 1,
-      "nome": "Pastilha de freio",
-      "preco": 120.00,
-    },
-    {
-      "id": 2,
-      "nome": "Filtro de óleo",
-      "preco": 35.90,
-    },
-    {
-      "id": 3,
-      "nome": "Filtro de ar",
-      "preco": 48.50,
-    },
-    {
-      "id": 4,
-      "nome": "Correia dentada",
-      "preco": 180.00,
-    },
-    {
-      "id": 5,
-      "nome": "Vela de ignição",
-      "preco": 25.00,
-    },
+    {"id": 1, "nome": "Pastilha de freio", "preco": 120.00},
+    {"id": 2, "nome": "Filtro de óleo", "preco": 35.90},
+    {"id": 3, "nome": "Filtro de ar", "preco": 48.50},
+    {"id": 4, "nome": "Correia dentada", "preco": 180.00},
+    {"id": 5, "nome": "Vela de ignição", "preco": 25.00},
   ];
 
   final List<Map<String, dynamic>> pecasDaOrdem = [];
@@ -84,10 +66,7 @@ class _OrderService extends State<OrderService> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 16,
-          ),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
           child: Column(
             children: [
               Container(
@@ -99,9 +78,7 @@ class _OrderService extends State<OrderService> {
                 child: Center(
                   child: Text(
                     "Placeholder do menu",
-                    style: TextStyle(
-                      color: colors.onSurfaceVariant,
-                    ),
+                    style: TextStyle(color: colors.onSurfaceVariant),
                   ),
                 ),
               ),
@@ -149,10 +126,7 @@ class _OrderService extends State<OrderService> {
                           return const SizedBox(height: 12);
                         },
                         itemBuilder: (context, index) {
-                          return _buildPieceCard(
-                            pecasDaOrdem[index],
-                            index,
-                          );
+                          return _buildPieceCard(pecasDaOrdem[index], index);
                         },
                       ),
               ),
@@ -175,9 +149,7 @@ class _OrderService extends State<OrderService> {
       backgroundColor: colors.surface,
       isScrollControlled: true,
       shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(24),
-        ),
+        borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
       ),
       builder: (context) {
         return SafeArea(
@@ -233,9 +205,7 @@ class _OrderService extends State<OrderService> {
                         ),
                         subtitle: Text(
                           "R\$${peca["preco"].toStringAsFixed(2)}",
-                          style: TextStyle(
-                            color: colors.onSurfaceVariant,
-                          ),
+                          style: TextStyle(color: colors.onSurfaceVariant),
                         ),
                         trailing: Icon(
                           Icons.chevron_right,
@@ -284,97 +254,110 @@ class _OrderService extends State<OrderService> {
     });
   }
 
-  Widget _buildPieceCard(
-    Map<String, dynamic> peca,
-    int index,
-  ) {
+  Widget _buildPieceCard(Map<String, dynamic> peca, int index) {
     final int quantidade = peca["quantidade"];
     final double preco = peca["preco"];
     final double totalItem = quantidade * preco;
 
-    return Container(
-      padding: const EdgeInsets.all(14),
-      decoration: BoxDecoration(
-        color: colors.surfaceContainer,
-        border: Border.all(
-          color: colors.primary,
-          width: 1.5,
-        ),
+   return Slidable(
+  key: ValueKey(peca["id"]),
+
+  startActionPane: ActionPane(
+    motion: const DrawerMotion(),
+    extentRatio: 0.25,
+    children: [
+      SlidableAction(
+        onPressed: (context) {
+          setState(() {
+            pecasDaOrdem.removeAt(index);
+          });
+        },
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.white,
+        icon: Icons.delete,
+        label: 'Excluir',
         borderRadius: BorderRadius.circular(18),
       ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    ],
+  ),
+
+      child: Container(
+        padding: const EdgeInsets.all(14),
+        decoration: BoxDecoration(
+          color: colors.surfaceContainer,
+          border: Border.all(color: colors.primary, width: 1.5),
+          borderRadius: BorderRadius.circular(18),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    peca["nome"],
+                    style: TextStyle(
+                      color: colors.onSurface,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+
+                  const SizedBox(height: 6),
+
+                  Text(
+                    "$quantidade un x R\$${preco.toStringAsFixed(2)}/un",
+                    style: TextStyle(
+                      color: colors.onSurfaceVariant,
+                      fontSize: 13,
+                    ),
+                  ),
+
+                  const SizedBox(height: 2),
+
+                  Text(
+                    "R\$${totalItem.toStringAsFixed(2)}",
+                    style: TextStyle(
+                      color: colors.onSurface,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Row(
               children: [
+                IconButton(
+                  onPressed: () => _aumentarQuantidade(index),
+                  icon: Icon(Icons.add_box_outlined, color: colors.primary),
+                ),
+
                 Text(
-                  peca["nome"],
+                  "$quantidade",
                   style: TextStyle(
                     color: colors.onSurface,
                     fontWeight: FontWeight.bold,
-                    fontSize: 18,
                   ),
                 ),
 
-                const SizedBox(height: 6),
-
-                Text(
-                  "$quantidade un x R\$${preco.toStringAsFixed(2)}/un",
-                  style: TextStyle(
-                    color: colors.onSurfaceVariant,
-                    fontSize: 13,
-                  ),
-                ),
-
-                const SizedBox(height: 2),
-
-                Text(
-                  "R\$${totalItem.toStringAsFixed(2)}",
-                  style: TextStyle(
-                    color: colors.onSurface,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
+                IconButton(
+                  onPressed: quantidade > 1
+                      ? () => _diminuirQuantidade(index)
+                      : null,
+                  icon: Icon(
+                    Icons.indeterminate_check_box_outlined,
+                    color: quantidade > 1
+                        ? colors.primary
+                        : colors.onSurfaceVariant,
                   ),
                 ),
               ],
             ),
-          ),
-
-          Row(
-            children: [
-              IconButton(
-                onPressed: () => _aumentarQuantidade(index),
-                icon: Icon(
-                  Icons.add_box_outlined,
-                  color: colors.primary,
-                ),
-              ),
-
-              Text(
-                "$quantidade",
-                style: TextStyle(
-                  color: colors.onSurface,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-              IconButton(
-                onPressed: quantidade > 1
-                    ? () => _diminuirQuantidade(index)
-                    : null,
-                icon: Icon(
-                  Icons.indeterminate_check_box_outlined,
-                  color: quantidade > 1
-                      ? colors.primary
-                      : colors.onSurfaceVariant,
-                ),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
-
