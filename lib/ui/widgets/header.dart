d02c5ca375fc9a801c46e954b367c9da3b341f3c
MariaBruning15 <mariaeduardafbruning@gmail.com>
2026-09-25 21:23:51 -0300

    fix(header): same estilization as the client_list topbar

--- lib/ui/widgets/header.dart
+++ lib/ui/widgets/header.dart
@@ -8,64 +8,39 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
   const Header({super.key, required this.onBack, required this.title});
 
   @override
-  Size get preferredSize => const Size.fromHeight(90);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
   Widget build(BuildContext context) {
     final colors = custom_colors.colorScheme;
 
     return AppBar(
-      toolbarHeight: 90,
       backgroundColor: Colors.transparent,
-      elevation: 0,
-      automaticallyImplyLeading: false,
-      title: Center(
-        child: Text(
-            title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+      foregroundColor: colors.onPrimary,
+      centerTitle: true,
+      elevation: 8,
+      shadowColor: Colors.black.withOpacity(0.5),
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: onBack,
       ),
-
+      title: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+      ),
       flexibleSpace: Container(
-        padding: const EdgeInsets.all(18),
-
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [colors.primary, colors.secondary],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 12,
-              offset: Offset(0, 6),
-            ),
-          ],
-        ),
-
-        child: SafeArea(
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: IconButton(
-                  onPressed: onBack,
-                  icon: const Icon(Icons.arrow_back, size: 20),
-                  style: IconButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.blue,
-                  ),
-                ),
-              ),
-
-              
+            colors: [
+              colors.primary,
+              colors.tertiary,
             ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
           ),
         ),
       ),
